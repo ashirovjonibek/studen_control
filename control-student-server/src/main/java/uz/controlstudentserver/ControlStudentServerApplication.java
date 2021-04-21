a2efc5adc4d534a9434263df719b4b30f34a91c0
Jonibek <ashirov.uzbek.1997@gmail.com>
2021-04-21 14:47:42 +0500

    adding server side

---
+++ control-student-server/src/main/java/uz/controlstudentserver/ControlStudentServerApplication.java
@@ -0,0 +1,13 @@
+package uz.controlstudentserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlStudentServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlStudentServerApplication.class, args);
+    }
+
+}
---
+++ control-student-server/src/test/java/uz/controlstudentserver/ControlStudentServerApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.controlstudentserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlStudentServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
