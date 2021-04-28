317a07afb2953b7227238f050bf0fc1e7aee5c72
Jonibek <ashirov.uzbek.1997@gmail.com>
2021-04-29 04:03:59 +0500

    Ending control-student-server

--- control-student-server/src/main/java/uz/controlstudentserver/controller/UserController.java
+++ control-student-server/src/main/java/uz/controlstudentserver/controller/UserController.java
@@ -7,8 +7,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import uz.controlstudentserver.dto.UserDto;
 import uz.controlstudentserver.entity.Role;
+import uz.controlstudentserver.entity.User;
 import uz.controlstudentserver.entity.enums.RoleName;
 import uz.controlstudentserver.payload.ApiResponse;
+import uz.controlstudentserver.payload.CurrentUser;
 import uz.controlstudentserver.servise.UserServise;
 
 import java.util.UUID;
@@ -22,36 +24,41 @@ public class UserController {
 
     @PreAuthorize("hasAnyRole({'ROLE_DEAN','ROLE_DEPUTY_DEAN'})")
     @GetMapping("/allUsers")
-    public HttpEntity<?> all(){
+    public HttpEntity<?> all() {
         ApiResponse all = userServise.getAll();
         return ResponseEntity.ok(all);
     }
 
     @PreAuthorize("hasAnyRole({'ROLE_DEAN','ROLE_DEPUTY_DEAN','ROLE_TEACHER'})")
     @GetMapping("/findUserByGroup")
-    public HttpEntity<?> findAllUsrByGroup(@RequestParam Integer groupId){
+    public HttpEntity<?> findAllUsrByGroup(@RequestParam Integer groupId) {
         ApiResponse response = userServise.findByUserGroupId(groupId);
         return ResponseEntity.ok(response);
     }
 
     @PreAuthorize("hasAnyRole({'ROLE_DEAN','ROLE_DEPUTY_DEAN'})")
     @GetMapping("/getByRoleName")
-    public HttpEntity<?> getByRoleName(@RequestParam Role role){
-       ApiResponse allUsersByRole = userServise.findAllUsersByRole(role.getRoleName());
-       return ResponseEntity.ok(allUsersByRole);
-   }
+    public HttpEntity<?> getByRoleName(@RequestParam Role role) {
+        ApiResponse allUsersByRole = userServise.findAllUsersByRole(role.getRoleName());
+        return ResponseEntity.ok(allUsersByRole);
+    }
 
     @PreAuthorize("hasAnyRole({'ROLE_ADMIN','ROLE_DEAN','ROLE_DEPUTY_DEAN'})")
     @PostMapping("/saveOrEdit")
-    public HttpEntity<?> saveOrEdit(@RequestBody UserDto dto){
-       ApiResponse response = userServise.saveOrEditUser(dto);
-       return ResponseEntity.ok(response);
-   }
+    public HttpEntity<?> saveOrEdit(@RequestBody UserDto dto) {
+        ApiResponse response = userServise.saveOrEditUser(dto);
+        return ResponseEntity.ok(response);
+    }
 
-   @PreAuthorize("hasAnyRole({'ROLE_ADMIN','ROLE_DEAN','ROLE_DEPUTY_DEAN','ROLE_TEACHER','ROLE_STUDENT'})")
-   @GetMapping("/findById/{id}")
-    public HttpEntity<?> findById(@PathVariable UUID id){
-       ApiResponse one = userServise.getOne(id);
-       return ResponseEntity.ok(one);
-   }
+    @PreAuthorize("hasAnyRole({'ROLE_ADMIN','ROLE_DEAN','ROLE_DEPUTY_DEAN','ROLE_TEACHER','ROLE_STUDENT'})")
+    @GetMapping("/findById/{id}")
+    public HttpEntity<?> findById(@PathVariable UUID id) {
+        ApiResponse one = userServise.getOne(id);
+        return ResponseEntity.ok(one);
+    }
+
+    @GetMapping("/me")
+    public HttpEntity<?> me(@CurrentUser User user) {
+        return ResponseEntity.status(user != null ? 200 : 409).body(user);
+    }
 }
